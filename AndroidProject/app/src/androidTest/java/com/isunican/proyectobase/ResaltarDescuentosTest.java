43406ae4a1895a50e875e49d346e4c6b5c8851a7
Miguel Casamichana <mcb653@alumnos.unican.es>
2020-11-06 00:28:11 +0100

    Pruebas de interfaz acabadas

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/ResaltarDescuentosTest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/ResaltarDescuentosTest.java
@@ -1,97 +1,42 @@
 package com.isunican.proyectobase;
 
-import android.app.Activity;
-import android.content.Context;
+
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import androidx.test.espresso.DataInteraction;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.espresso.intent.Checks;
-import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-
-import com.isunican.proyectobase.Model.Gasolinera;
-import com.isunican.proyectobase.Presenter.PresenterGasolineras;
 import com.isunican.proyectobase.Views.MainActivity;
-
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.w3c.dom.Text;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Objects;
-
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withChild;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.anything;
 
 @RunWith(AndroidJUnit4.class)
 public class ResaltarDescuentosTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-    /*
-    private static boolean netIsAvailable() {
-        try {
-            final URL url = new URL("http://www.google.com");
-            final URLConnection conn = url.openConnection();
-            conn.connect();
-            conn.getInputStream().close();
-            return true;
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            return false;
-        }
-    }
-
-    @Test
-    public void internetTest(){
-        Assert.assertEquals(netIsAvailable(),true);
-
-        try {
-            Assert.assertEquals(netIsAvailable(),false);
-            Assert.fail("Error de conexión");
-        }catch(Exception e){}
-    }*/
 
     /*
-        Se comprueba que la lista se cargue correctamente en el presenter
+        Se comprueba que la lista se cargue correctamente en la interfaz
      */
     @Test
     public void listaCargadaTest(){
-        PresenterGasolineras presenterGasolineras = new PresenterGasolineras();
-        presenterGasolineras.cargaDatosGasolineras();
-        Assert.assertNotEquals(presenterGasolineras.getGasolineras().size(),0);
+
+        try{
+            Thread.sleep(4000);
+        }catch(Exception e){}
+
+        ListAdapter adapter = mActivityTestRule.getActivity().adapter;
+        Assert.assertNotEquals(adapter.getCount(), 0);
     }
 
-    /*
+        /*
         Se comprueba que se muestre por la interfaz el color esperado de background de cada view y el color
         de letra esperado de los precios de cada una de las views que correspondan a las gasolineras con
         descuentos.
@@ -104,7 +49,6 @@ public class ResaltarDescuentosTest {
         } catch (InterruptedException e) {
 
         }
-        ListAdapter adapter = mActivityTestRule.getActivity().adapter;
         //Obtenemos la lista de vistas
         ListView lv = mActivityTestRule.getActivity().findViewById(R.id.listViewGasolineras);
 
