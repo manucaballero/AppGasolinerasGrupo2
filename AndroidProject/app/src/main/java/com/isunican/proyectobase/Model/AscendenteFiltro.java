417b6badce0f5fa4e0ef740d4b5c776ded590314
rukalrey <rcr251@alumnos.unican.es>
2020-11-11 22:26:23 +0100

    Implementados métodos ordenar filtros
    Han sido implementados todos los métodos de ordenación de los diferentes filtros creado, salvo dos que quedan por concretar, que son ascendente y descendente.
    También se ha añadido un atrbuto nombre a los filtros y su getter correspondiente.

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/Model/AscendenteFiltro.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Model/AscendenteFiltro.java
@@ -1,8 +1,20 @@
 package com.isunican.proyectobase.Model;
 
+import com.isunican.proyectobase.Presenter.PresenterGasolineras;
+
 public class AscendenteFiltro implements IPrecioFiltro {
 
-    public void ordena() {
+
+    String nombre = "ascendente";
+    @Override
+    public void ordena(PresenterGasolineras presenterGasolineras) {
 
     }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
 }
+
+
