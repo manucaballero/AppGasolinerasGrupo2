936d317cbb790e6baa971c1237ed5dd3a1e52ca1
bmc417 <bmc417@alumnos.unican.es>
2020-11-12 01:43:51 +0100

    Pruebas unitarias corregidas
    Se han corregido las pruebas unitarias de la clase PruebasUnitariasTest de acuerdo
    a las indicaciones dadas por la profesora de Procesos Software, de manera que cumplan
    los requisitos establecidos en el plan de pruebas.

--- AndroidProject/app/src/test/java/com/isunican/proyectobase/PruebasUnitariasTest.java
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/PruebasUnitariasTest.java
@@ -22,20 +22,30 @@ import static org.mockito.Mockito.when;
  */
 public class PruebasUnitariasTest {
 
-    PresenterGasolineras pg;
+    PresenterGasolineras sut;
     Gasolinera g1;
+    Gasolinera g2;
     double precio;
+    //lista desordenada
     List<Gasolinera> lista;
+    //lista desordenada con gasoleoA = 0
+    List<Gasolinera> l;
 
 
     @Before
     public void setUp(){
-        pg = new PresenterGasolineras();
-        pg.cargaDatosDummy();
-        lista = pg.getGasolineras();
-        g1 = pg.getGasolineras().get(0);
+        sut = new PresenterGasolineras();
+        sut.cargaDatosDummy();
+        lista = sut.getGasolineras();
+        g1 = sut.getGasolineras().get(0);
+        g2 = sut.getGasolineras().get(1);
         precio = g1.getGasoleoA();
-
+        l = new ArrayList<>();
+        l.add(new Gasolinera(1001,"SANTANDER","SANTANDER", "Av Valdecilla", 0,1.359,"AVIA","43.45741814","-3.82677519"));
+        l.add(new Gasolinera(1052,"SANTANDER","SANTANDER", "Plaza Matias Montero", 0,1.349,"CAMPSA","43.25741814","-3.84477519"));
+        l.add(new Gasolinera(421,"SANTANDER","SANTANDER", "Area Arrabal Puerto de Raos", 0,1.279,"E.E.S.S. MAS, S.L.","43.45741814","-3.82677519"));
+        l.add(new Gasolinera(9544,"SANTANDER","SANTANDER", "Av Parayas", 0,1.269,"EASYGAS","43.40741814","-3.92677519"));
+        l.add(new Gasolinera(1015,"SANTANDER","SANTANDER", "Calle el Empalme", 0,1.319,"CARREFOUR","43.42741814","-3.02677519"));
 
     }
 
@@ -50,25 +60,41 @@ public class PruebasUnitariasTest {
     
     @Test
     public void ordenaListaTest(){
-        pg.ordenaLista();
-        List<Gasolinera> lista = lista = pg.getGasolineras();
+        sut.ordenaLista();
+        List<Gasolinera> lista = lista = sut.getGasolineras();
 
         //Gasolineras ordenadas correctamente por precio
-        assertTrue(lista.get(0).getGasoleoA() <= lista.get(1).getGasoleoA());
-        assertTrue(lista.get(1).getGasoleoA() <= lista.get(2).getGasoleoA());
-        assertTrue(lista.get(2).getGasoleoA() <= lista.get(3).getGasoleoA());
-        assertTrue(lista.get(3).getGasoleoA() <= lista.get(4).getGasoleoA());
+        assertTrue(lista.get(0).getGasoleoAConDescuento() <= lista.get(1).getGasoleoAConDescuento());
+        assertTrue(lista.get(1).getGasoleoAConDescuento() <= lista.get(2).getGasoleoAConDescuento());
+        assertTrue(lista.get(2).getGasoleoAConDescuento() <= lista.get(3).getGasoleoAConDescuento());
+        assertTrue(lista.get(3).getGasoleoAConDescuento() <= lista.get(4).getGasoleoAConDescuento());
 
-        //Gasolineras no estan ordenadas por precio
+        sut.setGasolineras(l);
+        sut.ordenaLista();
+
+        //Gasolineras en el mismo orden que en la entrada
+        assertTrue(lista.get(0).getGasoleoAConDescuento() == l.get(0).getGasoleoAConDescuento());
+        assertTrue(lista.get(1).getGasoleoAConDescuento() == l.get(1).getGasoleoAConDescuento());
+        assertTrue(lista.get(2).getGasoleoAConDescuento() == l.get(2).getGasoleoAConDescuento());
+        assertTrue(lista.get(3).getGasoleoAConDescuento() == l.get(3).getGasoleoAConDescuento());
+        assertTrue(lista.get(4).getGasoleoAConDescuento() == l.get(4).getGasoleoAConDescuento());
 
     }
 
     @Test
     public void calcularPrecioFinalTest(){
+        g1.setTieneDescuento(true);
         g1.calculaPrecioFinal();
-        assertTrue(g1.getGasoleoA() == Gasolinera.round(precio*0.9+g1.getDistanciaEnKm()*6/100,4));
-    }
 
+        //Gasolinera con descuento
+        assertTrue(g1.getGasoleoAConDescuento() == Gasolinera.round((50*g1.getGasoleoA()+g1.getDistanciaEnKm()*6/100*g1.getGasoleoA())/50*0.9,3));
+
+        g2.setTieneDescuento(false);
+        g2.calculaPrecioFinal();
+
+        //Gasolinera sin descuento
+        assertTrue(g2.getGasoleoA() == Gasolinera.round((50*g2.getGasoleoA()+g2.getDistanciaEnKm()*6/100*g2.getGasoleoA())/50,3));
+    }
 
 
 }
