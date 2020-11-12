3c8b982f12506ce742c788741dec62cd07c83713
Daniel <dsd218@alumnos.unican.es>
2020-11-12 12:25:20 +0100

    Formulario funcional, falta añadir vehiculo nuevo a la lista

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/Views/FormActivity.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/Views/FormActivity.java
@@ -2,6 +2,8 @@ package com.isunican.proyectobase.Views;
 
 import com.isunican.proyectobase.Model.Vehiculo;
 import com.isunican.proyectobase.R;
+
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +12,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 /*
@@ -54,21 +56,51 @@ public class FormActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        Toast toast = null;
         Vehiculo v1 = null;
+
         if(v.getId()==R.id.txtAceptar){
-            v1 = new Vehiculo(campoModelo.getText().toString());
-            v1.setMatricula(campoMatricula.getText().toString());
-            v1.setDeposito(Double.parseDouble(campoCapacidad.getText().toString()));
 
+            String matricula = campoMatricula.getText().toString();
+            String modelo = campoModelo.getText().toString();
             String anotacion = campoAnotaciones.getText().toString();
-            if(!campoAnotaciones.equals(""))
-                v1.setAnotaciones(anotacion);
+            String capacidadtxt = campoCapacidad.getText().toString();
+            String consumoMediotxt = campoConsumomedio.getText().toString();
 
-            Double consumomedio = Double.parseDouble(campoConsumomedio.getText().toString());
-            if(consumomedio!=null)
-                v1.setConsumoMedio(consumomedio);
+            if(!modelo.equals("") && !capacidadtxt.equals("")){
+                v1 = new Vehiculo(modelo);
+                v1.setDeposito(Double.parseDouble(capacidadtxt));
+
+                if(!matricula.equals(""))
+                    v1.setMatricula(matricula);
+
+                if(!anotacion.equals(""))
+                    v1.setAnotaciones(anotacion);
+
+                if(!consumoMediotxt.equals(""))
+                    v1.setConsumoMedio(Double.parseDouble(consumoMediotxt));
+
+                //TODO AÑADIRLO A LA LUSTA Y ESCRIBIRLO
+                toast = Toast.makeText(getApplicationContext(), "Vehiculo añadido con exito", Toast.LENGTH_LONG);
+                toast.show();
+
+                Intent myIntent1 = new Intent(FormActivity.this, MainActivity.class);
+                FormActivity.this.startActivity(myIntent1);
+
+            }else {
+                toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.datos_no_obtenidos), Toast.LENGTH_LONG);
+                toast.show();
+                campoModelo.setText("Campo Requerido");
+                campoModelo.setTextColor(Color.RED);
+                campoCapacidad.setText("50");
+                campoCapacidad.setTextColor(Color.RED);
+                campoAnotaciones.setText("");
+                campoConsumomedio.setText("");
+                campoMatricula.setText("");
+            }
 
             //listaVehiculos.add(v1);
+
         }
         //guardaDatos();
 
