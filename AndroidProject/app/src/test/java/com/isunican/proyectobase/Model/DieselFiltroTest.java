97f395deb1c1baba2dd2fec550e25a858313a3e9
bmc417 <bmc417@alumnos.unican.es>
2020-11-19 20:38:59 +0100

    Test para el ordena de la clase DieselFiltro

---
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/Model/DieselFiltroTest.java
@@ -0,0 +1,82 @@
+package com.isunican.proyectobase.Model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class DieselFiltroTest {
+
+    IFiltro sut;
+    //lista desordenada
+    List<Gasolinera> gasolineraList;
+    //lista desordenada con gasóleo A == 0
+    List<Gasolinera> l;
+
+    Gasolinera g1;
+    Gasolinera g2;
+    Gasolinera g3;
+    Gasolinera g4;
+    Gasolinera g5;
+
+    /**
+     * Inicialización de variables
+     */
+    @Before
+    public void setUp(){
+        sut = new DieselFiltro();
+        l = new ArrayList<>();
+        gasolineraList = new ArrayList<>();
+
+        gasolineraList.add(new Gasolinera(1101,"SANTANDER","SANTANDER", "Av Valdecilla", 0.352,1.359,"AVIA","43.45741814","-3.82677519"));
+        gasolineraList.add(new Gasolinera(1152,"SANTANDER","SANTANDER", "Plaza Matias Montero", 1.034,1.349,"CAMPSA","43.25741814","-3.84477519"));
+        gasolineraList.add(new Gasolinera(411,"SANTANDER","SANTANDER", "Area Arrabal Puerto de Raos", 0.977,1.279,"E.E.S.S. MAS, S.L.","43.45741814","-3.82677519"));
+        gasolineraList.add(new Gasolinera(9144,"SANTANDER","SANTANDER", "Av Parayas", 1.543,1.269,"EASYGAS","43.40741814","-3.92677519"));
+        gasolineraList.add(new Gasolinera(1115,"SANTANDER","SANTANDER", "Calle el Empalme", 1.254,1.319,"CARREFOUR","43.42741814","-3.02677519"));
+
+
+        l.add(new Gasolinera(1001,"SANTANDER","SANTANDER", "Av Valdecilla", 0,1.359,"AVIA","43.45741814","-3.82677519"));
+        l.add(new Gasolinera(1052,"SANTANDER","SANTANDER", "Plaza Matias Montero", 0,1.349,"CAMPSA","43.25741814","-3.84477519"));
+        l.add(new Gasolinera(421,"SANTANDER","SANTANDER", "Area Arrabal Puerto de Raos", 0,1.279,"E.E.S.S. MAS, S.L.","43.45741814","-3.82677519"));
+        l.add(new Gasolinera(9544,"SANTANDER","SANTANDER", "Av Parayas", 0,1.269,"EASYGAS","43.40741814","-3.92677519"));
+        l.add(new Gasolinera(1015,"SANTANDER","SANTANDER", "Calle el Empalme", 0,1.319,"CARREFOUR","43.42741814","-3.02677519"));
+    }
+
+    /**
+     * Método test que comprueba que el método ordena() de la clase DieselFiltro funciona
+     * adecuadamente, ordenando la lista de gasolineras según el precio del gasóleoA.
+     */
+    @Test
+    public void ordenaGasoleATest(){
+
+        //lista con gasolineras y gasoleoA!=null
+        sut.ordena(gasolineraList);
+        assertEquals(true, gasolineraList.get(0).getGasoleoAConDescuento() <= gasolineraList.get(1).getGasoleoAConDescuento());
+        assertEquals(true, gasolineraList.get(1).getGasoleoAConDescuento() <= gasolineraList.get(2).getGasoleoAConDescuento());
+        assertEquals(true, gasolineraList.get(2).getGasoleoAConDescuento() <= gasolineraList.get(3).getGasoleoAConDescuento());
+        assertEquals(true, gasolineraList.get(3).getGasoleoAConDescuento() <= gasolineraList.get(4).getGasoleoAConDescuento());
+
+        //lista con gasolineras y gasoleoA==0
+        g1 = l.get(0);
+        g2 = l.get(1);
+        g3 = l.get(2);
+        g4 = l.get(3);
+        g5 = l.get(4);
+
+        sut.ordena(l);
+        assertEquals(g1.getGasoleoAConDescuento(), l.get(0).getGasoleoAConDescuento(), 0.0);
+        assertEquals(g2.getGasoleoAConDescuento(), l.get(1).getGasoleoAConDescuento(), 0.0);
+        assertEquals(g3.getGasoleoAConDescuento(), l.get(2).getGasoleoAConDescuento(), 0.0);
+        assertEquals(g4.getGasoleoAConDescuento(), l.get(3).getGasoleoAConDescuento(), 0.0);
+        assertEquals(g5.getGasoleoAConDescuento(), l.get(4).getGasoleoAConDescuento(), 0.0);
+
+        //lista vacía
+        l.clear();
+        sut.ordena(l);
+        assertEquals(0, l.size());
+    }
+
+}
