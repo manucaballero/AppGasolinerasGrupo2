9144c2270ba831c96c0f0f1f5e64f4235408c931
Miguel Casamichana <mcb653@alumnos.unican.es>
2020-11-30 20:02:38 +0100

    Anhadida clase de prueba del Pop Up

---
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/PopUpAnhadirVehiculoPorPrimeraVezUITest.java
@@ -0,0 +1,60 @@
+package com.isunican.proyectobase;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+
+
+import com.isunican.proyectobase.Views.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class PopUpAnhadirVehiculoPorPrimeraVezUITest {
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule(MainActivity.class);
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+    /**
+     * @author Miguel Casamichana Bolado
+     */
+    @Test
+    public void mostrarPopUpTest(){
+        openActionBarOverflowOrOptionsMenu(mActivityTestRule.getActivity().getApplicationContext());
+        try {
+            Thread.sleep(121000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withText("Actualizar")).perform(click());
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //onView(withId(R.id.texViewAnhadir)).check(matches(isDisplayed()));
+
+
+    }
+    /*@Test
+    public void testDoTheThing(){
+
+        MyClass myClass = new MyClass();
+        myClass.doTheThing();
+
+        await().atMost(2, Duration.SECONDS).until(didTheThing());  // Compliant
+        // assertions...
+    }*/
+}
