c4eb95eab75409263db3cb405d8e8fa0f9c01708
Aarón Rodríguez Solar <aaronrs18@gmail.com>
2020-12-02 12:23:11 +0100

    Realizado el test de interfaz DesaplicarFiltrosUITest

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/DesaplicarFiltrosUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/DesaplicarFiltrosUITest.java
@@ -3,12 +3,14 @@ package com.isunican.proyectobase;
 import android.Manifest;
 
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.GrantPermissionRule;
 
 import com.isunican.proyectobase.Views.MainActivity;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -19,7 +21,6 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class DesaplicarFiltrosUITest {
@@ -32,156 +33,80 @@ public class DesaplicarFiltrosUITest {
     MainActivity mActivity;
 
     @Before
-    public void setUp(){
-        mActivity=mActivityTestRule.getActivity();
-        mRecyclerView=mActivity.findViewById(R.id.recyclerViewFiltros);
+    public void setUp() {
+        mActivity = mActivityTestRule.getActivity();
+        mRecyclerView = mActivity.findViewById(R.id.recyclerViewFiltros);
     }
 
-
-    @Test
-    public void desaplicarFiltrosTest() throws NoSuchFieldException, IllegalAccessException {
-        onView(withId(R.id.buttonFiltrar)).perform(click());
-
-        //onView(withId(R.id.popUpFiltros)).check(matches(isDisplayed()));
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-        onView(withId(R.id.checkBoxDescuentoSi)).perform(click());
-        onView(withId(R.id.checkBoxGasoleoA)).perform(click());
-        onView(withId(R.id.buttonApply)).perform(click());
-
-        onView(withText("Con Descuento")).check(matches(isDisplayed()));
-        onView(withText("GasóleoA")).check(matches(isDisplayed()));
-
-
-        onView(withText("GasóleoA")).perform(click());
-
-        onView(withId(R.id.buttonSiBorrarFiltro)).perform(click());
-
-        onView(withText("Con Descuento")).check(matches(isDisplayed()));
-
-
-
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-
-
-
-        /*
-
-        //onView(withId(R.id.popUpFiltros)).check(matches(not(isDisplayed())));
-
-        //onView(withId(R.id.recyclerViewFiltros))
-        AdapterFiltros adapter=mActivityTestRule.getActivity().adapterFiltros;
-
-        //List<IFiltro> lista=adapter.getListaFiltros();
-
-        View v;
-        DataInteraction d;
-        IFiltro f;
-        for(int i=0; i<adapter.getItemCount(); i++){
-            d = onData(anything()).inAdapterView(withId(R.id.recyclerViewFiltros)).atPosition(i);
-            v = mRecyclerView.getChildAt(i);
-            //int id=v.getId();
-
-            //onView(withId(id)).check(matches(withText("Con Descuento")));
-
-            onView(withText("Con Descuento")).check(matches(isDisplayed()));
-
-            //d.onChildView(withText("GasóleoA")).check(matches(withText(f.getNombre())));
-
-            Log.d("desaplicarPrueba", "pasa por aquí ");
-
-
-        }
-
-
-        try {
-            Thread.sleep(4000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-
-
-
-    }
-
-
-
-    /*
     @Test
     public void desaplicarFiltrosTest(){
+        //Se pulsa el botón filtrar
         onView(withId(R.id.buttonFiltrar)).perform(click());
-        onView(withId(R.id.checkBoxGasoleoA)).perform(click());
-        onView(withId(R.id.checkBoxDescuentoSi)).perform(click());
-        onView(withId(R.id.buttonApply)).perform(click());
-
-        int itemCount=mRecyclerView.getAdapter().getItemCount();
-
-        if(itemCount > 0) {
-            for(int i=0; i < itemCount; i++) {
-
-                // clicking the item
-                onView(withId(R.id.recyclerViewFiltros))
-                        .perform(RecyclerViewActions.actionOnItemAtPosition(i, click());
-
-                // check if the ViewHolder is being displayed
-                onView(new RecyclerViewMatcher(this.resId)
-                        .atPositionOnView(i, R.id.cardview))
-                        .check(matches(isDisplayed()));
-
-                // checking for the text of the first one item
-                if(i == 0) {
-                    onView(new RecyclerViewMatcher(this.resId)
-                            .atPositionOnView(i, R.id.ingredientName))
-                            .check(matches(withText("Farbstoffe")));
-                }
-
-            }*/
-        }
-
-
-
-
-        /*
-        AdapterFiltros adapter=mActivityTestRule.getActivity().adapterFiltros;
-
-
-
-        IFiltro f;
-        DataInteraction d;
 
         try {
-            Thread.sleep(4000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        Log.d("desaplicarPrueba", "pasa por aquí "+adapter.getItemCount());
+        //Se elige filtrar por gasoleo A y con descuentos
+        onView(withId(R.id.checkBoxGasoleoA)).perform(click());
+        onView(withId(R.id.checkBoxDescuentoSi)).perform(click());
+        onView(withId(R.id.buttonApply)).perform(click());
 
-        List<IFiltro> lista=adapter.getListaFiltros();
+        //Se comprueba que los filtros que se muestran aplicados son 2
+        int itemCount = mRecyclerView.getAdapter().getItemCount();
+        Assert.assertEquals(itemCount,2);
 
-        for(int i=0; i<adapter.getItemCount(); i++){
-            d = onData(anything()).inAdapterView(withId(R.id.recyclerViewFiltros)).atPosition(i);
-            f = lista.get(i);
+        //Se compreba que la lista de filtros se muestra
+        onView(withId(R.id.recyclerViewFiltros)).check(matches(isDisplayed()));
 
-            //d.onChildView(withText("GasóleoA")).check(matches(withText(f.getNombre())));
+        //Se pulsa el primero, el que se quiere eliminar
+        onView(withId(R.id.recyclerViewFiltros))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
-            Log.d("desaplicarPrueba", "pasa por aquí "+f);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Se muestra un popup para confirmar que se quiere eliminar, se pulsa aceptar
+        onView(withId(R.id.buttonSiBorrarFiltro)).perform(click());
+
+        //Se comprueba que los filtros que se muestran aplicados son 1
+        itemCount = mRecyclerView.getAdapter().getItemCount();
+        Assert.assertEquals(itemCount,1);
+
+        //Se comprueba que la lista de filtros se muestra
+        onView(withId(R.id.recyclerViewFiltros)).check(matches(isDisplayed()));
 
 
-        }*/
+        /*
+        Ahora compruebo si al pulsar No Borrar en el pop-up, se mantiene el filtro
+         */
 
+        //Se pulsa el primero, el que se quiere eliminar
+        onView(withId(R.id.recyclerViewFiltros))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Se muestra un popup para confirmar que se quiere eliminar, se pulsa aceptar
+        onView(withId(R.id.buttonNoBorrarFiltro)).perform(click());
+
+        //Se comprueba que los filtros que se muestran aplicados son 1 (la misma cantidad que antes)
+        itemCount = mRecyclerView.getAdapter().getItemCount();
+        Assert.assertEquals(itemCount,1);
+
+        //Se comprueba que la lista de filtros se muestra
+        onView(withId(R.id.recyclerViewFiltros)).check(matches(isDisplayed()));
+    }
+}
 
 
 
