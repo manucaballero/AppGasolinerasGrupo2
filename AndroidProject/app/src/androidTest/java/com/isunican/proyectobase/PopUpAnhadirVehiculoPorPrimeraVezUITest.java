7d00e92a53445b92656c6cac27b596f127bcce3f
Miguel Casamichana <mcb653@alumnos.unican.es>
2020-12-02 18:42:57 +0100

    Actualizado trato de excepciones en el test

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/PopUpAnhadirVehiculoPorPrimeraVezUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/PopUpAnhadirVehiculoPorPrimeraVezUITest.java
@@ -1,14 +1,18 @@
 package com.isunican.proyectobase;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.intent.matcher.IntentMatchers;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.GrantPermissionRule;
 
 
 import com.isunican.proyectobase.Views.MainActivity;
+import com.isunican.proyectobase.Views.PopUpPrimerVehiculoActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -18,9 +22,12 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.junit.Assert.assertTrue;
 
 public class PopUpAnhadirVehiculoPorPrimeraVezUITest {
@@ -43,12 +50,18 @@ public class PopUpAnhadirVehiculoPorPrimeraVezUITest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //Si ya había saltado el pop up al iniciar se cierra
-        if(mActivityTestRule.getActivity().myIntentPop!=null) onView(withId(R.id.buttonMasTarde)).perform(click());
-        //Se actualiza la lista tras los 2 mimutos
-        openActionBarOverflowOrOptionsMenu(mActivityTestRule.getActivity().getApplicationContext());
+
+        try {
+            //Se actualiza la lista tras los 2 mimutos
+            openActionBarOverflowOrOptionsMenu(mActivityTestRule.getActivity().getApplicationContext());
+        }catch(NoMatchingViewException e) {
+            //Si ya había saltado el pop up al iniciar se cierra
+            onView(withId(R.id.buttonMasTarde)).perform(click());
+            openActionBarOverflowOrOptionsMenu(mActivityTestRule.getActivity().getApplicationContext());
+        }
         onView(withText("Actualizar")).perform(click());
         //Se comprueba que el pop up ha saltado como era de esperar
         onView(withId(R.id.texViewAnhadir)).check(matches(isDisplayed()));
+
     }
 }
